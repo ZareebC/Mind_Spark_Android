da8d97aa8b08d9e5849798bcef4f912498744419
Zareeb_Chowdhury <38160697+ZareebC@users.noreply.github.com>
2020-06-15 21:32:03 -0400

    first commit

---
+++ app/src/main/java/com/example/mindspark/MainArticlePage.java
@@ -0,0 +1,14 @@
+package com.example.mindspark;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainArticlePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_article_page);
+    }
+}
